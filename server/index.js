d03b417fbe6b3a0e9789c4d206be5b0fb4238310
Michał Studnicki <michalstudnicki8@gmail.com>
2021-05-21 17:59:34 +0200

    finished oauthgoogle

---
+++ server/index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const passport = require('passport');
+const cookieSession = require('cookie-session');
+require('./models/users');
+require('./services/passport');
+const keys = require('./config/keys');
+
+mongoose.connect(keys.mongoURI, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+});
+
+const app = express();
+
+app.use(cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
+    keys: [keys.cookieKey],
+}));
+app.use(passport.initialize()); // middleware
+app.use(passport.session()); // middleware
+
+require('./routes/authRoutes')(app);
+
+const port = process.env.PORT || 3000;
+app.listen(port);
---
+++ server/routes/authRoutes.js
@@ -0,0 +1,18 @@
+const passport = require('passport');
+
+module.exports = app => {
+    app.get('/auth/google', passport.authenticate('google', {
+        scope: ['profile', 'email'],
+    }))
+
+    app.get('/auth/google/callback', passport.authenticate('google'));
+
+    app.get('/api/logout', (req, res) => {
+        req.logout(); // logout function add to passport.js
+        res.send(req.user); // should be empty page - there is no req.user
+    })
+
+    app.get('/api/current_user', (req, res) => {
+        res.send(req.user); // show current loged in user
+    })
+};
