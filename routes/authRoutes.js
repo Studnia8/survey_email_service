9aa90438fafc1d475ee9aa716b7240cbc6a21fc2
Michał Studnicki <michalstudnicki8@gmail.com>
2021-05-22 12:42:56 +0200

    env test

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,12 +1,15 @@
 const passport = require('passport');
+const env = require('../config/keys');
 
 module.exports = app => {
     app.get('/auth/google', passport.authenticate('google', {
         scope: ['profile', 'email'],
-    }))
+    }));
 
     app.get('/auth/google/callback', passport.authenticate('google'));
-
+    app.get('/', (req, res) => {
+        res.send(env);
+    });
     app.get('/api/logout', (req, res) => {
         req.logout(); // logout function add to passport.js
         res.send(req.user); // should be empty page - there is no req.user
