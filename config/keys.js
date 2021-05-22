96078935a577fec4cc13d87c1945525e9ea02e49
Michał Studnicki <michalstudnicki8@gmail.com>
2021-05-22 12:47:02 +0200

    env test 2

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
 // keys management
-if (process.env.NODE_ENV.trim() === 'production') {
+if (process.env.NODE_ENV === 'production') {
     module.exports = require('./prod');
 } else {
     module.exports = require('./dev');
