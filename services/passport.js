2796737c363579ae552dac4c154a37d49015b951
Michał Studnicki <michalstudnicki8@gmail.com>
2021-05-30 18:31:19 +0200

    manage login/logout

--- services/passport.js
+++ services/passport.js
@@ -19,17 +19,15 @@ passport.use(new googleStrategy({ // google authorization
     clientSecret: keys.googleClientSecret,
     callbackURL: '/auth/google/callback',
     proxy: true,
-}, (accessToken, refreshToken, profile, done) => { // profile contains the data about user
-    User.findOne({ googleID: profile.id })
-        .then((existingUser) => {
-            if (existingUser) { // we already have a record in our database
-                done(null, existingUser);
-            } else {
-                new User({
-                        googleID: profile.id,
-                        displayedName: profile.displayName,
-                    }).save()
-                    .then(user => done(null, user)); // make sure user has been created and send done to passport
-            }
-        })
+}, async(accessToken, refreshToken, profile, done) => { // profile contains the data about user
+    const existingUser = await User.findOne({ googleID: profile.id });
+    if (existingUser) { // we already have a record in our database
+        done(null, existingUser);
+    } else {
+        const user = await new User({
+            googleID: profile.id,
+            displayedName: profile.displayName,
+        }).save();
+        done(null, user); // make sure user has been created and send done to passport
+    }
 }));
