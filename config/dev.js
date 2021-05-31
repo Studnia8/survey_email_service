53dcc763c6de46feb88a1be203737ae2ebb2ed5c
Michał Studnicki <michalstudnicki8@gmail.com>
2021-05-31 07:31:42 +0200

    manage login/logout

--- config/dev.js
+++
@@ -1,7 +0,0 @@
-// DEVELOPMENT
-module.exports = {
-    googleClientID: '105940631911-fouh2ueon88ra5t6cbktjbkmimnuvejp.apps.googleusercontent.com',
-    googleClientSecret: 'ARfLp3pi_n0kH5hx3QpeerCN',
-    mongoURI: 'mongodb+srv://admin:WgjPhbQjjp2qxT3J@database.hwft8.mongodb.net/emaily-dev?retryWrites=true&w=majority',
-    cookieKey: 'fsjoifdosaf78oiasjidjfasf',
-};
