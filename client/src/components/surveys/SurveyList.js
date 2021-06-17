6e9f4f0f1b2e34e7644995c8d4fceaab7c46bedb
Michał Studnicki <michalstudnicki8@gmail.com>
2021-06-17 13:24:52 +0200

    dashboard enhancement

--- client/src/components/surveys/SurveyList.js
+++ client/src/components/surveys/SurveyList.js
@@ -8,23 +8,37 @@ class SurveyList extends Component {
   }
 
   renderSurvey() {
-    return this.props.surveys.reverse().map((survey) => {
+    // make sure if user has no survey lead to create one
+    if (
+      this.props.surveys.length === 0 &&
+      !this.props.surveys.length === null
+    ) {
       return (
-        <div class="card blue-grey darken-1" key={survey._id}>
-          <div class="card-content white-text">
-            <span class="card-title">{survey.title}</span>
-            <p>{survey.body}</p>
-            <p className="right">
-              Sent on: {new Date(survey.lastResponded).toLocaleDateString()}
-            </p>
-          </div>
-          <div class="card-action">
-            <a className="black-text">Yes: {survey.yes}</a>
-            <a className="black-text">No: {survey.no}</a>
-          </div>
+        <div style={{ textAlign: "center" }}>
+          <h3>You have no surveys.</h3>
+          <p>Please click on add button on bottom right.</p>
         </div>
       );
-    });
+      // if user has survey display them
+    } else {
+      return this.props.surveys.reverse().map((survey) => {
+        return (
+          <div class="card blue-grey darken-1" key={survey._id}>
+            <div class="card-content white-text">
+              <span class="card-title">{survey.title}</span>
+              <p>{survey.body}</p>
+              <p className="right">
+                Sent on: {new Date(survey.lastResponded).toLocaleDateString()}
+              </p>
+            </div>
+            <div class="card-action">
+              <a className="black-text">Yes: {survey.yes}</a>
+              <a className="black-text">No: {survey.no}</a>
+            </div>
+          </div>
+        );
+      });
+    }
   }
 
   render() {
